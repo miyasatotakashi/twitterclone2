0b24530e54fc21ccfe76295646596c3232d75705
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-01 01:32:17 +0900

    ストロングパラメータでセキュリティ強化

--- app/controllers/ch2209s_controller.rb
+++ app/controllers/ch2209s_controller.rb
@@ -11,8 +11,14 @@ class Ch2209sController < ApplicationController
   end
 
   def create
-    Ch2209.create(comment: params[:ch2209][:comment])
+    Ch2209.create(ch2209_params)
     redirect_to new_ch2209_path
   end
 
+  private
+
+  def ch2209_params
+    params.require(:ch2209).permit(:comment)
+  end
+  
 end
