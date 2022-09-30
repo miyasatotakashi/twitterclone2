fee9ef6f80267de7512a2504509dbe8b7c539862
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-01 00:51:44 +0900

    ルーティング設定しビューヘルパーでフォームを作成

--- app/controllers/ch2209s_controller.rb
+++ app/controllers/ch2209s_controller.rb
@@ -1,4 +1,8 @@
 class Ch2209sController < ApplicationController
   def index
   end
+
+  def new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/ch2209s', to: 'ch2209s#index'
+  resources :ch2209s
 end
