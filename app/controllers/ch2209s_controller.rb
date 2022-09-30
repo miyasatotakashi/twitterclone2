c778eaf0ea42ba4d229378871424fc29aa9c8830
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-01 01:22:39 +0900

    ビューにデータを渡す

--- app/controllers/ch2209s_controller.rb
+++ app/controllers/ch2209s_controller.rb
@@ -2,13 +2,17 @@ class Ch2209sController < ApplicationController
   def index
   end
 
+  def show
+    redirect_to new_ch2209_path
+  end
+
   def new
     @ch2209 = Ch2209.new
   end
 
   def create
     Ch2209.create(comment: params[:ch2209][:comment])
-    redirect_to new_blog_path
+    redirect_to new_ch2209_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get '/ch2209s', to: 'ch2209s#index'
   resources :ch2209s
 end
